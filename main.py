28ac53fbe47937694508b7d1962be2e428dce171
antoninakus <antonina.kus123@gmail.com>
2024-12-10 17:44:17 +0100

    Add production 4

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tests import T5, T6, T8
 from tests.t1 import T1
 from tests.t2 import T2
 from tests.t3 import T3
+from tests.t4 import T4
 from tests.t13 import T13
 from tests.t14 import T14
 from tests.t15 import T15
@@ -11,6 +12,7 @@ if __name__ == "__main__":
     T1.run()
     T2.run()
     T3.run()
+    T4.run()
     T5.run()
     T6.run()
     T8.run()
