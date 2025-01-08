3392303b8fc72abc56ea10f3967d913ba029d5c2
MSSkowron <mateusz.skowron@poczta.onet.pl>
2025-01-08 18:30:03 +0100

    Add more test cases for P16

--- tests/t16.py
+++ tests/t16.py
@@ -9,11 +9,10 @@ from tests.test import Test
 class T16(Test):
     @staticmethod
     def run():
-        # Test Case 1: Production can be applied
         T16.case1()
-
-        # Test Case 2: Production cannot be applied (e.g., r=False)
-        # T16.case2()
+        T16.case2()
+        T16.case3()
+        T16.case4()
 
     @staticmethod
     def case1():
@@ -57,6 +56,82 @@ class T16(Test):
 
         # Create the hyperedge for the production
         hyperedge = HyperEdge((nodes[0], nodes[1], nodes[2], nodes[3], nodes[4]), "P", r=True)
+        g.add_hyper_edge(hyperedge)
+
+        draw(g)
+        P16().apply(g)
+        draw(g)
+
+    @staticmethod
+    def case3():
+        g = Graph()
+
+        # Create nodes
+        nodes = [
+            Node(1, 0, "N1"), #0
+            Node(4, 0, "N2"), #1
+            Node(4, 3, "N3"), #2
+            Node(1, 3, "N4"), #3
+            Node(0, 1.5, "N5"), #4
+            Node(2.5, -3, "N6"), #5
+            Node(2.5, 6, "N7"), #6
+        ]
+
+        for n in nodes:
+            g.add_node(n)
+
+        edges = [
+            (nodes[0], nodes[5]),
+            (nodes[1], nodes[5]),
+            (nodes[0], nodes[1]),
+            (nodes[3], nodes[6]),
+            (nodes[2], nodes[6]),
+            (nodes[2], nodes[3]),
+        ]
+
+        for u, v in edges:
+            g.add_edge(HyperEdge((u, v), "E"))
+
+        # Create the hyperedge for the production
+        hyperedge = HyperEdge((nodes[0], nodes[1], nodes[2], nodes[3], nodes[4]), "P", r=False)
+        g.add_hyper_edge(hyperedge)
+
+        draw(g)
+        P16().apply(g)
+        draw(g)
+
+    @staticmethod
+    def case4():
+        g = Graph()
+
+        # Create nodes
+        nodes = [
+            Node(1, 0, "N1"), #0
+            Node(4, 0, "N2"), #1
+            Node(4, 3, "N3"), #2
+            Node(1, 3, "N4"), #3
+            Node(0, 1.5, "N5"), #4
+            Node(2.5, -3, "N6"), #5
+            Node(2.5, 6, "N7"), #6
+        ]
+
+        for n in nodes:
+            g.add_node(n)
+
+        edges = [
+            (nodes[0], nodes[5]),
+            (nodes[1], nodes[5]),
+            (nodes[0], nodes[1]),
+            (nodes[3], nodes[6]),
+            (nodes[2], nodes[6]),
+            (nodes[2], nodes[3]),
+        ]
+
+        for u, v in edges:
+            g.add_edge(HyperEdge((u, v), "E"))
+
+        # Create the hyperedge for the production
+        hyperedge = HyperEdge((nodes[0], nodes[1], nodes[3], nodes[4]), "P", r=False)
         g.add_hyper_edge(hyperedge)
 
         draw(g)
