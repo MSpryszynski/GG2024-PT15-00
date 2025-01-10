43fe894e2b07cbe9cc7a64d8fae390444e0f91c9
Adam <adam.p.kulczycki@gmail.com>
2025-01-10 15:34:13 +0100

    update t7 test file

--- tests/t7.py
+++ tests/t7.py
@@ -23,3 +23,37 @@ class T7(Test):
         draw(g)
         P7().apply(g)
         draw(g)
+
+        g = Graph()
+        n1 = Node(0, 0, "N1", h=True)
+        n2 = Node(1, 0, "N2")
+        n3 = Node(1, 1, "N3")
+        n4 = Node(0, 1, "N4")
+        n5 = Node(0, 2, "N5")
+
+        for n in [n1, n2, n3, n4, n5]:
+            g.add_node(n)
+
+        g.add_hyper_edge(HyperEdge((n1, n2, n3, n4), "Q", r=False))
+        g.add_edge(HyperEdge((n1, n2), "E", True))
+
+        draw(g)
+        P7().apply(g)
+        draw(g)
+
+
+        g = Graph()
+        n1 = Node(0, 0, "N1", h=True)
+        n2 = Node(1, 0, "N2")
+        n3 = Node(1, 1, "N3")
+
+        for n in [n1, n2, n3]:
+            g.add_node(n)
+
+        g.add_hyper_edge(HyperEdge((n1, n2, n3), "Q", r=False))
+        HyperEdge((n2, n3), "E", True)
+        draw(g)
+        P7().apply(g)
+        draw(g)
+
+
