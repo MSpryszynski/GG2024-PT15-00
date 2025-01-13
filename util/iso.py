426d6204220442aa661978706efc42593cac7f0a
marcelspryszynski <marcel.spryszynski@gmail.com>
2025-01-13 23:40:52 +0100

    Project 2 starting commit [group 3]

--- util/iso.py
+++ util/iso.py
@@ -5,23 +5,25 @@ import networkx as nx
 def node_matcher(u, v):
     if u["h"] is None or v["h"] is None:
         return True
-    return u["h"] == v["h"] and u["hyper_r"] == v["hyper_r"]
+    return u["h"] == v["h"] and u["hyper_r"] == v["hyper_r"] and u["hyper"] == v["hyper"]
 
 
 def find_isomorphisms(graph: nx.Graph, subgraph: nx.Graph) -> list[dict]:
     matcher = GraphMatcher(graph, subgraph,
                            node_match=node_matcher)
-    processed_nodes = set()
-    mappings_to_process = []
+    distinct_mappings = []
+    seen_labels = set()
     mappings = sorted(list(matcher.subgraph_monomorphisms_iter()), key=lambda iso: "".join(node.label for node in iso.keys()))
     for iso_map in mappings:
-        nodes_of_found_mapping = set(iso_map.keys())
-        mapping_applicable = True
-        for node in nodes_of_found_mapping:
-            if node in processed_nodes:
-                mapping_applicable = False
-                break
-        if mapping_applicable:
-            mappings_to_process.append(iso_map)
-            processed_nodes.update(nodes_of_found_mapping)
-    return mappings_to_process
+        labels = tuple(sorted(node.label for node in iso_map.keys()))
+        if labels not in seen_labels:
+            seen_labels.add(labels)
+            distinct_mappings.append(iso_map)
+    for index, iso_map in enumerate(distinct_mappings):
+        labels = [node.label for node in iso_map.keys()]
+        print(f"Mapping {index}: {labels}")
+
+    selected_index = int(input("Pick the index of the mapping to process: "))
+    selected_mapping = distinct_mappings[selected_index]
+
+    return selected_mapping
